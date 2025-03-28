28515d8499be65b1c01e4d50e96d86847d102866
Pawan Rajak <pkpawanrajak@gmail.com>
2025-03-28 23:56:04 +0530

    Simplify CORS configuration in app.js and add test API for user login functionality

--- src/app.js
+++ src/app.js
@@ -13,10 +13,7 @@ import { ApiError } from "./utils/ApiError.js";
 const app = express();
 app.use(express.json())
 app.use(
-	cors({
-		origin: process.env.CORS_ORIGIN,
-		credentials: true,
-	})
+	cors()
 );
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
